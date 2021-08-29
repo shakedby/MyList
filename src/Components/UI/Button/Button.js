d5de88032158d06b6552bc7cfe078e88768ec5a7
shaked <by181996@gmail.com>
2021-08-29 14:43:57 +0300

    29/8

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -30,7 +30,7 @@ import styles from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button type={props.type} className={styles.Button} onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
